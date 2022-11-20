6bceccb118f0bbcc35107b5194cf541826df89e9
simon <skadel@posteo.de>
2022-11-20 17:51:41 +0100

    updates

---
+++ src/__test__/Event.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Event from "../Event";
+import { mockData } from "../mock-data";
+import { extractLocations } from "../api";
+
+
+describe("<EventSearch /> component", () => {
+    
+   
+  
+    test("render text input", () => {
+      const
+      expect().toHaveLength();
+    });
