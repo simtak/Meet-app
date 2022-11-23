3b157b1cdbb12a357ff71a324278b7ece44e39a4
simon <skadel@posteo.de>
2022-11-23 17:06:08 +0100

    integration testing

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
+
+class NumberOfEvents extends Component {
+  state = {
+    num: 32,
+    errorText: "",
+  };
+
+  handleInputChanged = (e) => {
+    const inputValue = e.target.value;
+
+    if (inputValue < 1 || inputValue > 32) {
+      this.setState({
+        infoText: "Please select number from 1 to 32",
+      });
+    } else {
+      this.props.updateEvents(null, inputValue);
+      this.setState({
+        eventCount: inputValue,
+        infoText: "",
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="NumberOfEvents">
+        <label for="number-of-events">Number of Events: </label>
+        <input
+          type="number"
+          id="number-of-events"
+          className="number"
+          min="1"
+          max="32"
+          value={this.state.eventCount}
+          onChange={this.handleInputChanged}
+        />
+        <ErrorAlert text={this.state.infoText} />
+      </div>
+    );
+  }
+}
+
+export default NumberOfEvents;
--- src/__test__/NumberOfEvents.test.js
+++ src/__test__/NumberOfEvents.test.js
@@ -1,3 +1,19 @@
 import React from "react";
 import { shallow } from "enzyme";
 import NumberOfEvents from "../NumberOfEvents.js";
+
+describe("<NumberOfEvents /> component", () => {
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+  });
+
+  test("render NumberOfEvents element", () => {
+    expect(NumberOfEventsWrapper.find(".NumberOfEvents")).toHaveLength(1);
+  });
+
+  test("input default value of 32", () => {
+    expect(NumberOfEventsWrapper.find("input.num").prop("type")).toBe("number");
+    expect(NumberOfEventsWrapper.state("num")).toBe(32);
+  });
+});
